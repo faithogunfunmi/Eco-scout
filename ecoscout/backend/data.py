5187e609302f5c4f119dd1f6614a9318bab4f753
espychacha-0 <esp.m.aguilar@gmail.com>
2026-02-28 13:25:45 -0800

    created basic database template

--- ecoscout/backend/data.py
+++ ecoscout/backend/data.py
@@ -1,5 +1,7 @@
 import firebase_admin
 from firebase_admin import credentials, firestore
 
-cred = credentials.Certificate("path/to/serviceAccountKey.json")
+cred = credentials.Certificate("credentials.json")
 firebase_admin.initialize_app(cred)
+
+db = firestore.client()
